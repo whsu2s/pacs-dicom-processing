33f0c13a2d56dbb1f14f9d4490b91bf3ddb0aeaa
whsu2s <weichiliedik@gmail.com>
2024-09-13 14:42:22 +0200

    Create filter_items.py

---
+++ filter_items.py
@@ -0,0 +1,21 @@
+anat_series = [
+    't1_mprage_sag_p2',
+    't1_mprage_sag_p2_iso',
+    't2_space_dark-fluid_sag',
+    't2_space_dark-fluid_sag_p2_iso',
+    't2_space_dark-fluid_sag_caipi4',
+]
+
+func_series = [
+    'ep2d_bold_resting_state'
+]
+
+diff_series = [
+    'ep2d_diff_DTI_mddw_30_p2_AP_SMS',
+    'ep2d_diff_3scan_trace_p2_ADC',
+    'ep2d_diff_4scan_trace_p2_ADC',
+    'b1000t',
+    'b1000',
+    'b_1ooot',
+    'b_1ooo'
+]
